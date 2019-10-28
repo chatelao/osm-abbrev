a6d671b25d8d355a83adf0c45037e72922454854
Olivier "Oli" Châtelain-Gmür <olivier.chatelain@dxc.com>
2019-10-28 20:26:31 +0100

    Add - Copy-Paste-Replace from giggls

---
+++ Makefile
@@ -0,0 +1,72 @@
+# 'Makefile'
+
+# Get extension version number from debian/changelog 
+EXTVERSION=$(shell head -n1 debian/changelog |cut -d \( -f 2 |cut -d \) -f 1)
+
+EXTDIR=$(shell pg_config --sharedir)
+
+SUBDIRS = kanjitranscript icutranslit
+CLEANDIRS = $(SUBDIRS:%=clean-%)
+INSTALLDIRS = $(SUBDIRS:%=install-%)
+
+all: $(patsubst %.md,%.html,$(wildcard *.md)) INSTALL README Makefile $(SUBDIRS) osmabbrv.control country_languages.data  osmabbrv_country_osm_grid.data
+
+INSTALL: INSTALL.md
+	pandoc --from markdown_github --to plain --standalone $< --output $@
+
+README: README.md
+	pandoc --from markdown_github --to plain --standalone $< --output $@
+
+%.html: %.md
+	pandoc --from markdown_github --to html --standalone $< --output $@
+
+.PHONY:	subdirs $(SUBDIRS)
+      
+subdirs:
+	$(SUBDIRS)
+                
+$(SUBDIRS):
+	$(MAKE) -C $@
+
+# I have no idea how to use the Makefile from "pg_config --pgxs"
+# for installation without interfering mine
+# so will do it manually (fo now)
+install: $(INSTALLDIRS) all 
+	mkdir -p $(DESTDIR)$(EXTDIR)/extension
+	install -D -c -m 644 osmabbrv--$(EXTVERSION).sql $(DESTDIR)$(EXTDIR)/extension/
+	install -D -c -m 644 osmabbrv.control $(DESTDIR)$(EXTDIR)/extension/
+	install -D -c -m 644 osmabbrv_thai_transcript--$(EXTVERSION).sql $(DESTDIR)$(EXTDIR)/extension/
+	install -D -c -m 644 osmabbrv_thai_transcript.control $(DESTDIR)$(EXTDIR)/extension/
+	install -D -c -m 644 *.data $(DESTDIR)$(EXTDIR)/extension/
+
+$(INSTALLDIRS):
+	$(MAKE) -C $(@:install-%=%) install
+
+deb:
+	dpkg-buildpackage -b -us -uc
+
+clean: $(CLEANDIRS)
+	rm -rf $$(grep -v country_osm_grid.sql .gitignore)
+	
+# remove everything including the files from the interwebs
+mrproper: clean
+	rm country_osm_grid.sql
+	rm country_languages.data
+	
+$(CLEANDIRS):
+	$(MAKE) -C $(@:clean-%=%) clean
+
+osmabbrv--$(EXTVERSION).sql: plpgsql/*.sql country_languages.data
+	./gen_osmabbrv_extension.sh $(EXTDIR)/extension $(EXTVERSION)
+	
+osmabbrv_thai_transcript--$(EXTVERSION).sql: thaitranscript/*.sql
+	./gen_osmabbrv_thai_extension.sh $(EXTDIR)/extension $(EXTVERSION)
+
+osmabbrv.control: osmabbrv--$(EXTVERSION).sql
+	sed -e "s/VERSION/$(EXTVERSION)/g" osmabbrv.control.in >osmabbrv.control
+
+osmabbrv_thai_transcript.control: osmabbrv_thai_transcript--$(EXTVERSION).sql
+	sed -e "s/VERSION/$(EXTVERSION)/g" osmabbrv_thai_transcript.control.in >osmabbrv_thai_transcript.control
+
+country_languages.data:
+	grep -v \# country_languages.data.in >country_languages.data
---
+++ plpgsql/street_abbrv.sql
@@ -0,0 +1,285 @@
+/*
+
+renderer independent name abbreviations
+
+- https://github.com/chatelao/osm-abbrev
+
+(c) 2014-2016 Sven Geggus
+(c) 2019      Olivier Chatelain, olivier.chatelain(ät)gmail.com
+
+Licence AGPL http://www.gnu.org/licenses/agpl-3.0.de.html
+
+Street abbreviation functions
+
+*/
+
+/* 
+   helper function "osmabbrv_street_abbrev"
+   will call the osmabbrv_street_abbrev function of the given language if available
+   and return the unmodified input otherwise   
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev(longname text, langcode text) RETURNS TEXT AS $$
+ DECLARE
+  call text;
+  func text;
+  result text;
+ BEGIN
+  IF (position('-' in langcode)>0) THEN
+    return longname;
+  END IF;
+  IF (position('_' in langcode)>0) THEN
+    return longname;
+  END IF;  
+  func ='osmabbrv_street_abbrev_'|| langcode;
+  call = 'select ' || func || '(' || quote_nullable(longname) || ')';
+  execute call into result;
+  return result;
+ EXCEPTION
+  WHEN undefined_function THEN
+   return longname;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_all"
+   call all osmabbrv_street_abbrev functions
+   These are currently russian, english and german
+   
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_all(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=osmabbrv_street_abbrev_en(longname);
+  abbrev=osmabbrv_street_abbrev_de(abbrev);
+  abbrev=osmabbrv_street_abbrev_fr(abbrev);
+  abbrev=osmabbrv_street_abbrev_nl(abbrev);
+  abbrev=osmabbrv_street_abbrev_ru(abbrev);
+  abbrev=osmabbrv_street_abbrev_uk(abbrev);
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_all_latin"
+   call all latin osmabbrv_street_abbrev functions
+   These are currently: english and german
+   
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_latin(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=osmabbrv_street_abbrev_en(longname);
+  abbrev=osmabbrv_street_abbrev_de(abbrev);
+  abbrev=osmabbrv_street_abbrev_fr(abbrev);
+  abbrev=osmabbrv_street_abbrev_nl(abbrev);
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_non_latin"
+   call all non latin osmabbrv_street_abbrev functions
+   These are currently: russian, ukrainian
+   
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_non_latin(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=osmabbrv_street_abbrev_ru(longname);
+  abbrev=osmabbrv_street_abbrev_uk(abbrev);
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+
+
+/* 
+   helper function "osmabbrv_street_abbrev_de"
+   replaces some common parts of german street names with their abbr
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_de(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=longname;
+  IF (position('traße' in abbrev)>2) THEN
+   abbrev=regexp_replace(abbrev,'Straße\M','Str.');
+   abbrev=regexp_replace(abbrev,'straße\M','str.');
+  END IF;
+  IF (position('asse' in abbrev)>2) THEN
+   abbrev=regexp_replace(abbrev,'Strasse\M','Str.');
+   abbrev=regexp_replace(abbrev,'strasse\M','str.');
+   abbrev=regexp_replace(abbrev,'Gasse\M','G.');
+   abbrev=regexp_replace(abbrev,'gasse\M','g.');
+  END IF;
+  IF (position('latz' in abbrev)>2) THEN
+   abbrev=regexp_replace(abbrev,'Platz\M','Pl.');
+   abbrev=regexp_replace(abbrev,'platz\M','pl.');
+  END IF;
+  IF (position('Professor' in abbrev)>0) THEN
+   abbrev=replace(abbrev,'Professor ','Prof. ');
+   abbrev=replace(abbrev,'Professor-','Prof.-');
+  END IF;
+  IF (position('Doktor' in abbrev)>0) THEN
+   abbrev=replace(abbrev,'Doktor ','Dr. ');
+   abbrev=replace(abbrev,'Doktor-','Dr.-');
+  END IF;
+  IF (position('Bürgermeister' in abbrev)>0) THEN
+   abbrev=replace(abbrev,'Bürgermeister ','Bgm. ');
+   abbrev=replace(abbrev,'Bürgermeister-','Bgm.-');
+  END IF;
+  IF (position('Sankt' in abbrev)>0) THEN
+   abbrev=replace(abbrev,'Sankt ','St. ');
+   abbrev=replace(abbrev,'Sankt-','St.-');
+  END IF;
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_fr"
+   replaces some common parts of french street names with their abbreviation
+   Main source: https://www.canadapost.ca/tools/pg/manual/PGaddress-f.asp#1460716
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_fr(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=longname;
+  abbrev=regexp_replace(abbrev,'^Avenue\M','Av.');
+  abbrev=regexp_replace(abbrev,'^Boulevard\M','Bd');
+  abbrev=regexp_replace(abbrev,'^Chemin\M','Ch.');
+  abbrev=regexp_replace(abbrev,'^Esplanade\M','Espl.');
+  abbrev=regexp_replace(abbrev,'^Impasse\M','Imp.');
+  abbrev=regexp_replace(abbrev,'^Passage\M','Pass.');
+  abbrev=regexp_replace(abbrev,'^Promenade\M','Prom.');
+  abbrev=regexp_replace(abbrev,'^Route\M','Rte');
+  abbrev=regexp_replace(abbrev,'^Ruelle\M','Rle');
+  abbrev=regexp_replace(abbrev,'^Sentier\M','Sent.');
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_es"
+   replaces some common parts of spanish street names with their abbreviation
+   currently just a stub :(
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_es(longname text) RETURNS TEXT AS $$
+ BEGIN
+  return longname;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_pt"
+   replaces some common parts of portuguese street names with their abbreviation
+   currently just a stub :(
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_pt(longname text) RETURNS TEXT AS $$
+ BEGIN
+  return longname;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_pt"
+   replaces some common parts of portuguese street names with their abbreviation
+   currently just a stub :(
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_nl(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=longname;
+  abbrev=regexp_replace(abbrev,'straat\M','str.');
+  abbrev=regexp_replace(abbrev,'Sint\M','St.');
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_en"
+   replaces some common parts of english street names with their abbreviation
+   Most common abbreviations extracted from:
+   http://www.ponderweasel.com/whats-the-difference-between-an-ave-rd-st-ln-dr-way-pl-blvd-etc/
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_en(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=longname;
+  abbrev=regexp_replace(abbrev,'(?!^)Avenue\M','Ave.');
+  abbrev=regexp_replace(abbrev,'(?!^)Boulevard\M','Blvd.');
+  abbrev=regexp_replace(abbrev,'Crescent\M','Cres.');
+  abbrev=regexp_replace(abbrev,'Court\M','Ct');
+  abbrev=regexp_replace(abbrev,'Drive\M','Dr.');
+  abbrev=regexp_replace(abbrev,'Lane\M','Ln.');
+  abbrev=regexp_replace(abbrev,'Place\M','Pl.');
+  abbrev=regexp_replace(abbrev,'Road\M','Rd.');
+  abbrev=regexp_replace(abbrev,'Street\M','St.');
+  abbrev=regexp_replace(abbrev,'Square\M','Sq.');
+
+  abbrev=regexp_replace(abbrev,'Expressway\M','Expy');
+  abbrev=regexp_replace(abbrev,'Freeway\M','Fwy');
+  abbrev=regexp_replace(abbrev,'Highway\M','Hwy');
+  abbrev=regexp_replace(abbrev,'Parkway\M','Pkwy');
+
+  abbrev=regexp_replace(abbrev,'North\M','N');
+  abbrev=regexp_replace(abbrev,'South\M','S');
+  abbrev=regexp_replace(abbrev,'West\M', 'W');
+  abbrev=regexp_replace(abbrev,'East\M', 'E');
+
+  abbrev=regexp_replace(abbrev,'Northwest\M', 'NW');
+  abbrev=regexp_replace(abbrev,'Northeast\M', 'NE');
+  abbrev=regexp_replace(abbrev,'Southwest\M', 'SW');
+  abbrev=regexp_replace(abbrev,'Southeast\M', 'SE');
+
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+
+
+/* 
+   helper function "osmabbrv_street_abbrev_ru"
+   replaces улица (ulica) with ул. (ul.)
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_ru(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=regexp_replace(longname,'переулок','пер.');
+  abbrev=regexp_replace(abbrev,'тупик','туп.');
+  abbrev=regexp_replace(abbrev,'улица','ул.');
+  abbrev=regexp_replace(abbrev,'бульвар','бул.');
+  abbrev=regexp_replace(abbrev,'площадь','пл.');
+  abbrev=regexp_replace(abbrev,'проспект','просп.');
+  abbrev=regexp_replace(abbrev,'спуск','сп.');
+  abbrev=regexp_replace(abbrev,'набережная','наб.');
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
+
+/* 
+   helper function "osmabbrv_street_abbrev_uk"
+   replaces ukrainian street suffixes with their abbreviations
+*/
+CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_uk(longname text) RETURNS TEXT AS $$
+ DECLARE
+  abbrev text;
+ BEGIN
+  abbrev=regexp_replace(longname,'провулок','пров.');
+  abbrev=regexp_replace(abbrev,'тупик','туп.');
+  abbrev=regexp_replace(abbrev,'вулиця','вул.');
+  abbrev=regexp_replace(abbrev,'бульвар','бул.');
+  abbrev=regexp_replace(abbrev,'площа','пл.');
+  abbrev=regexp_replace(abbrev,'проспект','просп.');
+  abbrev=regexp_replace(abbrev,'спуск','сп.');
+  abbrev=regexp_replace(abbrev,'набережна','наб.');
+  return abbrev;
+ END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;
