8cc375d273b4542cc0db194481772c1625f30f59
Olivier "Oli" Châtelain-Gmür <olivier.chatelain@dxc.com>
2019-11-12 20:32:59 +0100

    Chg - Fix the Avenues

--- plpgsql/street_abbrv.sql
+++ plpgsql/street_abbrv.sql
@@ -158,7 +158,7 @@ CREATE or REPLACE FUNCTION osmabbrv_street_abbrev_fr(longname text) RETURNS TEXT
   abbrev text;
  BEGIN
   abbrev=longname;
-  abbrev=regexp_replace(abbrev,'^[0-9]*(r)?e Avenue\M','Av.');
+  abbrev=regexp_replace(abbrev,'^([0-9]+(r)?e )?Avenue\M','Av.');
   abbrev=regexp_replace(abbrev,'^Boulevard\M','Bd');
   abbrev=regexp_replace(abbrev,'^Chemin\M','Ch.');
   abbrev=regexp_replace(abbrev,'^Esplanade\M','Espl.');
