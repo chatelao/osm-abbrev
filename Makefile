625d87ae218081764259f58f1fc6cd4b1a93d312
Olivier "Oli" Châtelain-Gmür <olivier.chatelain@dxc.com>
2019-11-13 18:26:30 +0100

    Chg - Next try to detect current postgre version

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@
 # Get extension version number from debian/changelog 
 EXTVERSION=$(shell head -n1 debian/changelog |cut -d \( -f 2 |cut -d \) -f 1)
 # EXTDIR=$(shell sudo -u postgres pg_config --sharedir)
-EXTDIR=/usr/share/postgresql/10
+EXTDIR=/usr/share/postgresql/$(shell pg_lsclusters | grep online | cut -d " " -f 1 )
 
-# SUBDIRS = kanjitranscript icutranslit
 CLEANDIRS = $(SUBDIRS:%=clean-%)
 INSTALLDIRS = $(SUBDIRS:%=install-%)
 
--- gen_osmabbrv_extension.sh
+++ gen_osmabbrv_extension.sh
@@ -16,21 +16,6 @@ for cmd in curl sed basename; do
   fi
 done
 
-# # download country_osm_grid.sql from nominatim if not available
-# if ! [ -f "country_osm_grid.sql" ]; then
-#   rm -f country_osm_grid.sql
-#   echo -n "Trying to download country_grid.sql.gz from nominatim.org... "
-#   curl -s http://www.nominatim.org/data/country_grid.sql.gz |gzip -d >country_osm_grid.sql
-
-#   if ! [ -s country_osm_grid.sql ]; then
-#     rm -f country_osm_grid.sql
-#     echo "failed."
-#     exit 1
-#   else
-#     echo "done."
-#   fi
-# fi
-
 SCRIPTS=plpgsql/*
 
 (
